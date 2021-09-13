2fe8d4701523b3e2da5e9b739491cc8d89a65f2d
Jason Robbins <jasonrobbins@jasons-mbp-2.lan>
2021-09-12 22:00:03 -0400

    Added a Review object class: Review.cs.

---
+++ ReviewGeneratorAPI/Review.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ReviewGeneratorAPI
+{
+    public class Review
+    {
+        public int StarRating { get; set; }
+        public string Overall { get; set; }
+        public bool Verified { get; set; }
+        public string ReviewTime { get; set; }
+        public string ReviewerID { get; set; }
+        public string ASIN { get; set; }
+        public string ReviewerName { get; set; }
+        public string ReviewText { get; set; }
+        public string Summary { get; set; }
+    }
+}
